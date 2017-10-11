b3467409ea3d42734bbb0472a6f81fd03ef2cbd9
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-10 21:02:24 -0500

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,7 +5,7 @@ let salesforce = require('./salesforce'),
     formatter = require('./formatter'),
     postbacks = require('./postbacks');
 
-/*exports.hi = (sender) => {
+exports.hi = (sender) => {
     messenger.getUserInfo(sender).then(response => {
         messenger.send({text: `Hello, ${response.first_name}!`}, sender);
     });
@@ -27,26 +27,4 @@ exports.searchProducts_Offers = (sender) => {       
     salesforce.searchProducts().then(product => {               
         messenger.send(formatter.formatproduct(product), sender);        
     });
-};//End HEB Code ************************** End HEB Code*/
-
-exports.hi = (sender) => {
-    messenger.getUserInfo(sender).then(response => {
-        messenger.send({text: `Hello, ${response.first_name}!`}, sender);
-    });
 };
-
-exports.help = (sender) => {
-    messenger.send({text: `You can ask me questions like "Survey Ticket 1234", "Ticket 1234"`}, sender);
-};
-
-exports.answerSurvey = (sender, values) => {        
-    messenger.send({text: `Survey for Ticket ${values[1]}`}, sender);  
-    messenger.send(formatter.start_Question(), sender); 
-};
-
-exports.addCommentsText = (sender, values) => { 
-    messenger.getUserInfo(sender).then(response => {
-        postbacks.end_quiz(sender, values);
-    });
-};//End Tate And Lyle Code ************************** End Tate And Lyle Code*/
-
