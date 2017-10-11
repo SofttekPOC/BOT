8ec25ad18d53ac6cee890f0ef17c1e73f8097d09
HEB <31109925+SofttekPOC@users.noreply.github.com>
2017-10-10 21:03:21 -0500

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -7,7 +7,7 @@ let salesforce = require('./salesforce'),
 let global_variable = [];
 let addComments = 0;
 
-/*exports.schedule_visit = (sender, values) => {
+exports.schedule_visit = (sender, values) => {
     salesforce.findProperties({id: values[1]}).then(properties => {
         messenger.send(formatter.formatAppointment(properties[0]), sender);
     });
@@ -56,53 +56,4 @@ exports.quiz = (sender, values) => {
 			messenger.send({text: `Thank you for answering our survey. Your comments are very much appreciated.`}, sender); 
 		}); 
 	});
-};//End HEB Code ************************** End HEB Code*/
-
-exports.start_quiz = (sender, values) => {
-    messenger.send({text: "Answer the next questions..."}, sender);
-    messenger.send(formatter.TAL_Question_1(), sender);
 };
-
-exports.quiz_1 = (sender, values) => { 
-    global_variable[1] = values[1];
-    messenger.send(formatter.TAL_Question_2(), sender);
-};
-
-exports.quiz_2 = (sender, values) => { 
-    global_variable[2] = values[1];
-    messenger.send(formatter.TAL_Question_3(), sender);
-};
-
-exports.quiz_3 = (sender, values) => { 
-    global_variable[3] = values[1];
-    messenger.send(formatter.TAL_Question_4(), sender);
-};
-
-exports.quiz_4 = (sender, values) => { 
-    global_variable[4] = values[1];
-    messenger.send(formatter.TAL_Question_5(), sender);
-};
-
-exports.quiz_5 = (sender, values) => { 
-    global_variable[5] = values[1];
-    addComments = 1;
-    messenger.send(formatter.end_Question(), sender);
-};
-
-exports.quiz_6 = (sender) => { 
-    messenger.send(formatter.add_Comments(), sender);
-};
-
-exports.end_quiz = (sender, values) => { 
-    if(addComments == 1){
-	global_variable[6] = values[1];  
-    } 
-    messenger.getUserInfo(sender).then(response => { 
-        salesforce.createSurvey(global_variable, response.first_name + " " + response.last_name, sender, addComments).then(() => { 
-	    if(addComments == 1){
-	        messenger.send({text: `Thank you for answering our survey. Your comments are very much appreciated.`}, sender); 
-		addComments = 0;
-	    }
-	}); 
-    });
-};//End Tate And Lyle Code ************************** End Tate And Lyle Code*/
